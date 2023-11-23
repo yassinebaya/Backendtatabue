44a16c4ad22a8c0acf63382bb7907b062617d676
yassinebaya <yassinebenayad@gmail.com>
2023-11-23 14:37:09 +0100

    update TestController

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -16,7 +16,7 @@ import java.util.List;
 @CrossOrigin("*")
 public class TestController {
     @GetMapping("/Test")
-    public String demerrertest() {
+    public  Object[] demerrertest() {
         Object[] partnerIds = new Object[0];
         try {
             XmlRpcClientConfigImpl config = new XmlRpcClientConfigImpl();
@@ -59,7 +59,7 @@ public class TestController {
             e.printStackTrace();
         }
 
-        return "yassssss";
+        return partnerIds;
     }
 
 }
--- src/main/java/com/example/demo/security/SecurityConfig.java
+++ src/main/java/com/example/demo/security/SecurityConfig.java
@@ -5,8 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -17,6 +20,9 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+
+                .sessionManagement(sm->sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .csrf(csrf->csrf.disable())
                 .authorizeHttpRequests(authConfig -> {
                     authConfig.requestMatchers("/").permitAll();
                     authConfig.requestMatchers("/Test/**").authenticated();
@@ -29,17 +35,23 @@ public class SecurityConfig {
     }
     @Bean
     UserDetailsService userDetailsService() {
+        PasswordEncoder passwordEncoder1=passwordEncoder();
+
         var admin = User.builder()
                 .username("Willy De Keyser")
-                .password("{noop}password")
+                .password(("{noop}password"))
                 .roles("USER", "ADMIN")
                 .build();
         var user = User.builder()
                 .username("Ken De Keyser")
-                .password("{noop}password")
+                .password(( passwordEncoder1.encode("password")))
                 .roles("USER")
                 .build();
         return new InMemoryUserDetailsManager(admin, user);
+    }
+@Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 
 }
