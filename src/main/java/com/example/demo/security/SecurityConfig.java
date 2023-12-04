216d1c57ed1a9ff407fee39647762315a2d6e23e
yassinebaya <yassinebenayad@gmail.com>
2023-12-04 09:39:14 +0100

    update test45

--- src/main/java/com/example/demo/security/SecurityConfig.java
+++ src/main/java/com/example/demo/security/SecurityConfig.java
@@ -1,17 +1,29 @@
 package com.example.demo.security;
 
+import com.nimbusds.jose.jwk.source.ImmutableSecret;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.crypto.spec.SecretKeySpec;
 
 @Configuration
 @EnableWebSecurity
@@ -23,17 +35,21 @@ public class SecurityConfig {
 
                 .sessionManagement(sm->sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .csrf(csrf->csrf.disable())
-               /* .authorizeHttpRequests(authConfig -> {
+                .authorizeHttpRequests(authConfig -> {
                     authConfig.requestMatchers("/").permitAll();
-                    authConfig.requestMatchers("/Test/**").authenticated();
+                    authConfig.requestMatchers("/login/**").permitAll();
                     authConfig.requestMatchers("/admin/**").denyAll();
 
-                })*/
-                .authorizeHttpRequests(ar->ar.anyRequest().authenticated())
+                })
+               // .authorizeHttpRequests(ar->ar.anyRequest().authenticated())
+                .oauth2ResourceServer(oa->oa.jwt(Customizer.withDefaults()));
                // .formLogin(Customizer.withDefaults()) // Login with browser and Form
-                .httpBasic(Customizer.withDefaults()); // Login with Insomnia and Basic Auth
+              //  .httpBasic(Customizer.withDefaults()); // Login with Insomnia and Basic Auth
+
         return http.build();
     }
+
+
     @Bean
     UserDetailsService userDetailsService() {
         PasswordEncoder passwordEncoder1=passwordEncoder();
@@ -50,10 +66,31 @@ public class SecurityConfig {
                 .build();
         return new InMemoryUserDetailsManager(admin, user);
     }
-@Bean
+    @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
+
+    @Bean
+    JwtEncoder JwtEncoder (){
+        String secrykey="404E635266556A586E3272357538782F413F4428472B4B6250645367566B5970";
+        return new NimbusJwtEncoder(new ImmutableSecret<>(secrykey.getBytes()));
+
+    }
+    @Bean
+    JwtDecoder jwtDecoder(){
+        String secrykey="404E635266556A586E3272357538782F413F4428472B4B6250645367566B5970";
+        SecretKeySpec secretKeySpec=new SecretKeySpec(secrykey.getBytes(),"RSA");
+        return NimbusJwtDecoder.withSecretKey(secretKeySpec).macAlgorithm(MacAlgorithm.HS512).build();
+    }
+    @Bean
+    public AuthenticationManager authenticationManager(UserDetailsService userDetailsService){
+        DaoAuthenticationProvider daoAuthenticationProvider=new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+        daoAuthenticationProvider.setUserDetailsService(userDetailsService);
+        return new ProviderManager( daoAuthenticationProvider);
+    }
+
 
 }
 
