b2eeee0e9fb38e4c1ba67b43c8255cb256adc1c8
yassinebaya <57502644+yassinebaya@users.noreply.github.com>
2023-12-21 10:27:15 +0100

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,6 +4,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin("*")
 @SpringBootApplication
 public class DemoApplication extends SpringBootServletInitializer{
 	public static void main(String[] args) {
@@ -14,5 +20,9 @@ public class DemoApplication extends SpringBootServletInitializer{
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
         return builder.sources(DemoApplication.class);
     }
+	@GetMapping("/Test2")
+	public String demarres(){
+		return "bravo";
+	}
 
 }
