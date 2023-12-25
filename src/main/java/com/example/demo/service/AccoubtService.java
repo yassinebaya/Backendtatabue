128f8f9b00713f2d2d521fc9353c6856ea4395e8
yassinebaya <57502644+yassinebaya@users.noreply.github.com>
2023-12-25 11:01:19 +0100

    Update AccoubtService.java

--- src/main/java/com/example/demo/service/AccoubtService.java
+++ src/main/java/com/example/demo/service/AccoubtService.java
@@ -3,9 +3,10 @@ package com.example.demo.service;
 import com.example.demo.entites.AppRole;
 import com.example.demo.entites.AppUser;
 
-public interface AccoubtService {
+public interface AccoubtService  {
     AppUser addNewUser(String username);
     AppRole addnewRole(String rolname);
-
+    void addRoleToUser();
+    void removeRoleFromUser(String username,String Role);
 
 }
