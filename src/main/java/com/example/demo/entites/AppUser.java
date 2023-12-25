c440cd998e1fb61f8e778e49674dc08b83354bc7
yassinebaya <57502644+yassinebaya@users.noreply.github.com>
2023-12-25 11:07:40 +0100

    update

--- src/main/java/com/example/demo/entites/AppUser.java
+++ src/main/java/com/example/demo/entites/AppUser.java
@@ -2,6 +2,7 @@ package com.example.demo.entites;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -11,6 +12,7 @@ import java.util.Collection;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class AppUser {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
---
+++ src/main/java/com/example/demo/service/Accountserviceimpl.java
@@ -0,0 +1,32 @@
+package com.example.demo.service;
+
+import com.example.demo.entites.AppRole;
+import com.example.demo.entites.AppUser;
+
+public class Accountserviceimpl implements AccoubtService {
+
+    @Override
+    public AppUser addNewUser(String username) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'addNewUser'");
+    }
+
+    @Override
+    public AppRole addnewRole(String rolname) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'addnewRole'");
+    }
+
+    @Override
+    public void addRoleToUser() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'addRoleToUser'");
+    }
+
+    @Override
+    public void removeRoleFromUser(String username, String Role) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'removeRoleFromUser'");
+    }
+    
+}
