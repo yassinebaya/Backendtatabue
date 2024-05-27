d4e303e86d4d040e4685ac233216451b7c5b19a8
yassinebaya <57502644+yassinebaya@users.noreply.github.com>
2024-05-27 18:56:58 +0100

    update

--- src/main/java/com/example/demo/security/SecurityConfig.java
+++ src/main/java/com/example/demo/security/SecurityConfig.java
@@ -132,7 +132,7 @@ public class SecurityConfig {
 @Bean
 CorsConfigurationSource corsConfigurationSource(){
     CorsConfiguration corsConfiguration=new CorsConfiguration();
-    corsConfiguration.addAllowedOrigin("http://localhost:4200"); // Autorise seulement cette origine
+    corsConfiguration.addAllowedOrigin("http://docutrackproximity.s3-website.eu-north-1.amazonaws.com"); // Autorise seulement cette origine
     corsConfiguration.addAllowedMethod("*");
     corsConfiguration.addAllowedHeader("*");
     corsConfiguration.setAllowCredentials(true); // Autorise les informations d'authentification
